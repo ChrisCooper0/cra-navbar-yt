cb67af27f09e6575b31cbbbe0dd8382983f9b2e6
ChrisCooper0 <christopher.cooper@mail.com>
2022-05-08 18:36:37 +0100

    horizontal-scroll-bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <Navbar />;
+  const onScroll = () => {
+    let pixelsFromTop = window.scrollY;
+    let documentHeight = document.body.clientHeight;
+    let windowHeight = window.innerHeight;
+    let difference = documentHeight - windowHeight;
+    let percentage = (100 * pixelsFromTop) / difference;
+    document.getElementById("bar").style.width = `${percentage}%`;
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  });
+
+  return (
+    <div className="page">
+      <Navbar />
+      <div className="progress_wrapper">
+        <div className="progress_bar" id="bar"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

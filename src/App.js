5de6a8ef4549700aaae62d2122e75bc6e5db2a58
ChrisCooper0 <christopher.cooper@mail.com>
2022-05-18 18:07:53 +0100

    add scroll to top button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { FaArrowUp } from "react-icons/fa";
 
 function App() {
+  const [showButton, setShowButton] = useState(true);
   const onScroll = () => {
     let pixelsFromTop = window.scrollY;
     let documentHeight = document.body.clientHeight;
@@ -10,6 +12,9 @@ function App() {
     let difference = documentHeight - windowHeight;
     let percentage = (100 * pixelsFromTop) / difference;
     document.getElementById("bar").style.width = `${percentage}%`;
+
+    // Scroll to top button logic
+    window.scrollY > 500 ? setShowButton(true) : setShowButton(false);
   };
 
   useEffect(() => {
@@ -17,12 +22,20 @@ function App() {
     return () => window.removeEventListener("scroll", onScroll);
   });
 
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <div className="page">
       <Navbar />
       <div className="progress_wrapper">
         <div className="progress_bar" id="bar"></div>
       </div>
+      <FaArrowUp
+        className={showButton ? "showButton" : "hidden"}
+        onClick={scrollToTop}
+      />
     </div>
   );
 }
